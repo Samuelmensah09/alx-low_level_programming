21fdeed9bb756b4dba1f674a7ed7187320eb1adf
Samuelmensah09 <qbasic270@gmail.com>
2023-06-30 22:44:35 -0700

    _putchar

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,33 +1,23 @@
 #include "main.h"
 /**
- * strncpy - copy a string 
- * @dest input value
- * @src input value
- * @n input value
- *
- * Return dest
- */
- char *_strncpy(char *dest, char *src, int n);
+*_strncpy - a function that copies a string.
+*at most n bytes of src are copied.
+*if length of src is < n, additional null bytes are written to dest
+*to ensure that a total of n bytes are written.
+*@dest: pointer to destination string.
+*@src: pointer to source string.
+*@n: number of bytes to be used.
+*
+* Return: pointer to destination string.
+*/
+char *_strncpy(char *dest, char *src, int n)
 {
-        int j;
+	int byteCount;
 
-        j=0
-        while() <n&& src[j] !='\0')
-        {
-                i++;
-        }
+	for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
+	dest[byteCount] = src[byteCount];
+	for (; byteCount < n; byteCount++)
+	dest[byteCount] = '\0';
 
-        j  = 0;
-        while (src[j] != '/0')
-        {
-                dest[i] = src[j];
-                j++;
-
-        }
-	while(j < n)
-	{
-	dest[i] = '0';
-         j++;
-	}
-        return(dest);
+	return (dest);
 }
